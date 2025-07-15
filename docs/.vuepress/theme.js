fb2e7af3830d81a4b033b3f391032c707e742c05
Sakana <admin@yangrucheng.top>
2025-07-15 21:27:57 +0800

    chore: docs

--- docs/.vuepress/theme.js
+++ docs/.vuepress/theme.js
@@ -3,7 +3,7 @@ import { hopeTheme } from "vuepress-theme-hope";
 export default hopeTheme(
     {
         docsDir: "docs",
-        logo: "https://avatars.githubusercontent.com/u/90495619?v=4",
+        logo: "https://github.com/WAADRI.png",
         repo: "Misaka-1314/SignNode-AutoBuild",
         hostname: "https://doc.waadri.top",
 
@@ -11,50 +11,42 @@ export default hopeTheme(
             name: "Misaka",
             url: "https://github.com/Misaka-1314",
         },
+        navbar: ["/guide/", "/faq/"],
 
         pageInfo: ["Author", "Original", "Date", "Category", "Tag", "ReadingTime"],
 
         markdown: {
-            imgMark: true,      //支持图片标记
-            imgLazyload: true,  //支持图片懒加载
-            imgSize: true,      //支持图片大小
-            tabs: true,         //支持表格
-            gfm: true,          //支持完整的 GFM 语法
-            tasklist: true,     //支持任务列表
-            include: true,      //支持 include 语法
-            align: true,        //支持对齐
-            mark: true,         //支持标记
-            sub: true,          //支持下标
-            sup: true,          //支持上标
-            demo: true,         //支持 demo
-            plantuml: true,     //支持 PlantUML
-            codeTabs: true,     //支持代码块分组
+            imgMark: true,      // 支持图片标记
+            imgLazyload: true,  // 支持图片懒加载
+            imgSize: true,      // 支持图片大小
+            tabs: true,         // 支持表格
+            gfm: true,          // 支持完整的 GFM 语法
+            tasklist: true,     // 支持任务列表
+            include: true,      // 支持 include 语法
+            align: true,        // 支持对齐
+            mark: true,         // 支持标记
+            sub: true,          // 支持下标
+            sup: true,          // 支持上标
+            demo: true,         // 支持 demo
+            plantuml: true,     // 支持 PlantUML
+            codeTabs: true,     // 支持代码块分组
         },
         plugins: {
-            // 搜索从设置转移到主题
-            docsearch: {
-                appId: "WNF44MLO3Y",
-                apiKey: "783936724dafe33256a4388c113d8099",
-                indexName: "doc_waadri_top_wnf44mlo3y_pages",
+            // 本地搜索
+            search: {
+                locales: {
+                    "/": {
+                        placeholder: "搜索文档~♡",
+                    },
+                },
             },
             // 图标
             icon: {
                 assets: [
-                    "//at.alicdn.com/t/c/font_2410206_5vb9zlyghj.css",
+                    "https://at.alicdn.com/t/c/font_2410206_5vb9zlyghj.css",
                     "https://npm.elemecdn.com/font6pro@6.4.0/css/fontawesome.min.css",
                     "https://npm.elemecdn.com/font6pro@6.4.0/css/all.min.css",
                 ]
-            },
-            comment: {
-                /**
-                 * Using Giscus 评论
-                 */
-                provider: "Giscus",
-                repo: "Misaka-1314/SignNode-AutoBuild",
-                repoId: "R_kgDONKu_jQ",
-                category: "Announcements",
-                categoryId: "DIC_kwDONKu_jc4CoWNm",
-
             },
             // 临时弹窗
             notice: [
@@ -66,6 +58,31 @@ export default hopeTheme(
                     showOnce: true,
                 }
             ],
+            // 水印
+            watermark: {
+                watermarkOptions: {
+                    content: "WAADRI 免费的学习通签到平台",
+                    movable: true,
+                },
+            },
+            // PWA
+            pwa: {
+                manifest: {
+                    name: "WAADRI 免费的学习通签到平台",
+                    short_name: "WAADRI 学习通签到",
+                    start_url: "/",
+                    icons: [{
+                        src: 'https://github.com/WAADRI.png',
+                        sizes: "460x460",
+                        type: "image/png",
+                    }],
+                },
+                cacheImage: true,
+                cacheHTML: true,
+                update: "force",
+            },
+            // SEO
+            seo: true,
         },
     },
     {
