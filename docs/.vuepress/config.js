54fd31a41e4e8a1a05cd54a5c7f8739e08d3cdfa
Sakana <admin@yangrucheng.top>
2025-07-15 19:11:27 +0800

    chore: docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,22 +4,29 @@ import { defineUserConfig } from 'vuepress'
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  bundler: viteBundler({
-    viteOptions: {},
-    vuePluginOptions: {},
-  }),
-  theme: theme,
-  base: "/./",
-  head: [
-    ['link', { rel: 'icon', href: 'https://avatars.githubusercontent.com/u/90495619?v=4' }]
-    ['meta', { name: 'algolia-site-verification', content: '85D4DB2CA351BEC9' }],
-    ['meta', { name: 'referrer', content: 'never' }],
-  ],
-  locales: {
-    "/": {
-      lang: "zh-CN",
-      title: "WAADRI 文档",
-      description: "WAADRI 超新星学习通在线自动签到抢答系统 文档",
+    bundler: viteBundler({
+        viteOptions: {
+            optimizeDeps: {
+                include: ['naive-ui']
+            },
+            ssr: {
+                noExternal: ['naive-ui']
+            }
+        }
+    }),
+    theme: theme,
+    base: "/./",
+    head: [
+        ['link', { rel: 'icon', href: 'https://github.com/WAADRI.png' }],
+        ['meta', { name: 'referrer', content: 'never' }],
+        ['script', { type: 'text/javascript', src: '/js/analyze.js' }],
+    ],
+    locales: {
+        "/": {
+            lang: "zh-CN",
+            title: "WAADRI 文档",
+            description: "WAADRI 超新星学习通在线自动签到抢答系统 文档",
+        },
     },
-  },
+    plugins: [],
 })
